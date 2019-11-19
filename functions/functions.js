6bc68d9c253f254188ada746a84300cd0930fc21
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-19 12:02:58 -0500

    created function, declared variables

---
+++ functions/functions.js
@@ -0,0 +1,16 @@
+//https://codeguppy.com/code.html?t=_functions
+
+// let x = 400;
+// let y = 300;
+// let h = 100;
+
+function drawHouse () {
+
+    let x = 400;
+    let y = 300;
+    let h = 100;
+
+    rect(x - h / 2, y - h / 2, h, h);
+    line(x - h / 2, y - h / 2, x, y - h / 2 - h / 2);
+    line(x, y - h / 2 - h / 2, x + h / 2, y - h / 2);
+}
